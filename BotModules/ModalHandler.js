3e66a54b7739ea8a36f80e98d936dccaa66ab8f5
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2022-07-28 09:03:07 +0100

    Create Modal Handler + add Modal Error MSGs

---
+++ BotModules/ModalHandler.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const { DiscordClient, Collections } = await import("../constants.js");
+const LocalizedErrors = await import("../JsonFiles/errorMessages.json");
+const Config = await import("../config.js");
+
+module.exports = {
+    /**
+     * Handles and runs received Modals
+     * @param {Discord.ModalSubmitInteraction} modalInteraction 
+     * @returns {Promise<*>}
+     */
+    async Main(modalInteraction)
+    {
+        // Grab first part of Custom ID
+        const ModalCustomId = modalInteraction.customId.split("_").shift();
+        const Modal = Collections.Modals.get(ModalCustomId)
+
+        if ( !Modal )
+        {
+            // Couldn't find the file for this Modal
+            return await modalInteraction.reply({ ephemeral: true, content: LocalizedErrors[modalInteraction.locale].MODAL_GENERIC_FAILED_RARE });
+        }
+
+
+        // Attempt to run Modal
+        try { await Modal.execute(modalInteraction); }
+        catch (err)
+        {
+            //console.error(err);
+            if ( modalInteraction.deferred )
+            {
+                await modalInteraction.editReply({ content: LocalizedErrors[modalInteraction.locale].MODAL_GENERIC_FAILED });
+            }
+            else
+            {
+                await modalInteraction.reply({ ephemeral: true, content: LocalizedErrors[modalInteraction.locale].MODAL_GENERIC_FAILED });
+            }
+        }
+
+        return;
+    }
+}
