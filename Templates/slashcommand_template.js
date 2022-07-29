9127206529d73f016d32d3bdc1410c637cc98a86
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2022-07-29 13:41:27 +0100

    Create slashcommand_template.js

---
+++ Templates/slashcommand_template.js
@@ -0,0 +1,69 @@
+const Discord = require("discord.js");
+const { DiscordClient, Collections } = await import("../constants.js");
+const LocalizedErrors = await import("../JsonFiles/errorMessages.json");
+const LocalizedStrings = await import("../JsonFiles/stringMessages.json");
+
+module.exports = {
+    // Command's Name
+    //     Use full lowercase
+    Name: "commandname",
+
+    // Command's Description
+    Description: `Description`,
+
+    // Command's Category
+    Category: "GENERAL",
+
+    // Cooldown, in seconds
+    //     Defaults to 3 seconds if missing
+    Cooldown: 3,
+
+    // Cooldowns for specific subcommands and/or subcommand-groups
+    //     IF SUBCOMMAND: name as "subcommandName"
+    //     IF SUBCOMMAND GROUP: name as "subcommandGroupName_subcommandName"
+    SubcommandCooldown: {
+        "example": 3
+    },
+
+    // Scope of Command's usage
+    //     One of the following: DM, GUILD
+    Scope: "GUILD",
+
+
+
+    /**
+     * Returns data needed for registering Slash Command onto Discord's API
+     * @returns {Discord.ChatInputApplicationCommandData}
+     */
+    registerData()
+    {
+        /** @type {Discord.ChatInputApplicationCommandData} */
+        const Data = {};
+
+        Data.name = this.Name;
+        Data.description = this.Description;
+        Data.type = Discord.ApplicationCommandType.ChatInput;
+    },
+
+
+
+    /**
+     * Executes the Slash Command
+     * @param {Discord.ChatInputCommandInteraction} slashCommand 
+     */
+    async execute(slashCommand)
+    {
+        //.
+    },
+
+
+
+    /**
+     * Handles given Autocomplete Interactions for any Options in this Slash CMD that uses it
+     * @param {Discord.AutocompleteInteraction} autocompleteInteraction 
+     */
+    async autocomplete(autocompleteInteraction)
+    {
+        //.
+    }
+}
