ae495b8b1658d147c46a5163f379e56f014d1e63
Zebby <48347420+TwilightZebby@users.noreply.github.com>
2022-07-29 09:43:34 +0100

    Add Autocomplete handler

---
+++ BotModules/AutocompleteHandler.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const { DiscordClient, Collections } = await import("../constants.js");
+const LocalizedErrors = await import("../JsonFiles/errorMessages.json");
+const Config = await import("../config.js");
+
+module.exports = {
+    /**
+     * Handles and runs received Autocomplete Interactions
+     * @param {Discord.AutocompleteInteraction} autocompleteInteraction 
+     * @returns {Promise<*>}
+     */
+    async Main(autocompleteInteraction)
+    {
+        // Find Slash Command with matching name
+        const SlashCommand = Collections.SlashCommands.get(autocompleteInteraction.commandName);
+        if ( !SlashCommand ) { return await autocompleteInteraction.respond([{name: LocalizedErrors[autocompleteInteraction.locale].AUTOCOMPLETE_GENERIC_FAILED, value: "ERROR_FAILED"}]); }
+
+        // Pass to Command's Autocomplete Method
+        return await SlashCommand.autocomplete(autocompleteInteraction);
+    }
+}
